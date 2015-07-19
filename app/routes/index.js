04e493c624987f31a7eff0f8e0db7bae8fa2c7ea
Abbas Monfared <a@monfa.red>
2015-07-19 09:49:30 -0400

    working on mongoose

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// connect to mongodb
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/chat');
+
+
+// Schema
+var personSchema = {
+  firstName: String,
+  lastName: String,
+  email: String
+}
+
+var Person = mongoose.model('person', personSchema, 'people')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Person.find(function (err, doc) {
+    res.send(doc);
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
