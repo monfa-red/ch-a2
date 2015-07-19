44ff78fa21d1ff5eaa60de0197a96fbcb5c5db63
Abbas Monfared <a@monfa.red>
2015-07-19 17:48:14 +0000

    added users controller, just testing now :)

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,34 @@
+/**
+ * Module dependencie
+ */
+var mongoose = require('mongoose');
+var User = mongoose.model('User')
+
+
+
+
+ /**
+  * CRUD 
+  */
+
+// create
+
+// read
+
+// update
+
+// delete
+
+/**
+ * List users
+ * Just a test!
+ */
+ module.exports.list = function(req, res) {
+ 	User.find(function(err, users) {
+ 		if (err) {
+ 			console.log(err);
+ 			res.redirect('/');
+ 		}
+ 		res.json(users);
+ 	})
+ }
