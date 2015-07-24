6d99305aaaac97c9b0b3a3a5ee09320a56a941f7
Abbas Monfared <a@monfa.red>
2015-07-24 21:31:27 +0000

    user controller CRUD boilerplate

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -7,31 +7,49 @@ var User = mongoose.model('User');
 
 
 
- /**
-  * CRUD 
-  */
 
-// create
+ module.exports = {
 
-// read
-
-// update
-
-// delete
-
-/**
- * List users
- * Just a test!
- */
- module.exports.list = function(req, res) {
-    User
-        .find()
-        .exec(function(err, users) {
+    /**
+     * CRUD
+     */
+    create: function(req, res) {
+        newUser = new User(req.body);
+        newUser.save(function(err) {
             if (err) {
                 console.log(err);
-                res.redirect('/');
-            }
-            // console.log(users);
-            res.json(users);
-        });
- };
+                // res.redirect()
+            };
+            // if succesfull
+            res.json(newUser)
+
+        })     
+    },
+    read: function(req, res) {
+
+    },
+    update: function(req, res) {
+
+    },
+    delete: function(req, res) {
+
+    },
+
+    /**
+     * List users
+     * Just a test!
+     */
+    list: function(req, res) {
+        User
+            .find(function(err, result) {
+                if (err) {
+                    // log the error --REMOVE-THIS--
+                    console.log(err);
+                    res.redirect('/error-page');
+                }
+                // if succesfull
+                res.json(result);
+            })
+     }
+
+ }
